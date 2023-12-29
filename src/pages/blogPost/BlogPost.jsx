73b71aff76f72344035996ed1d68b96139bc603a
RufusSchultz <rufusschultz@gmail.com>
2023-12-29 12:48:41 +0100

    Bonusopdracht gemaakt.

--- src/pages/blogPost/BlogPost.jsx
+++ src/pages/blogPost/BlogPost.jsx
@@ -1,13 +1,14 @@
 import "./BlogPost.css";
 import {useParams} from "react-router-dom";
 import dateFormatter from "../../helpers/dateFormatter.js";
-import { CaretLeft } from "@phosphor-icons/react";
+import { CaretLeft, X } from "@phosphor-icons/react";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 function BlogPost() {
     const [postInfo, setPostInfo] = useState({});
     const [errorMessage, setErrorMessage] = useState("");
+    const [deleteMessage, setDeleteMessage] = useState("");
     const { postId } = useParams();
 
     useEffect(() => {
@@ -23,20 +24,36 @@ function BlogPost() {
         fetchPost();
     }, []);
 
+    async function deletePost() {
+        try {
+            const result = await axios.delete(`http://localhost:3000/posts/${postId}`);
+            console.log(result);
+            setDeleteMessage("Post succesvol verwijderd!");
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     return (
         <div className="page-container">
-            {Object.keys(postInfo).length === 0 && <div className="inner-container">
-                <h2 className="error_message">{errorMessage}</h2>
+            {deleteMessage.length === 0 && Object.keys(postInfo).length === 0 && <div className="inner-container">
+                <h2 className="message">{errorMessage}</h2>
             </div>}
-            {Object.keys(postInfo).length > 0 && <div className="inner-container">
+            {deleteMessage.length === 0 && Object.keys(postInfo).length > 0 && <div className="inner-container">
                 <article>
                     <h2>{postInfo.title} ({postInfo.readTime} minuten)</h2>
                     <h3>{postInfo.subtitle}</h3>
                     <p>Geschreven door {postInfo.author} op {dateFormatter(postInfo.created)}</p>
                     <p>{postInfo.content}</p>
                     <p>{postInfo.comments} reacties - {postInfo.shares} keer gedeeld</p>
-                    <p><a href="/overview" className="return_home_link"><CaretLeft />Terug naar de overzichtspagina</a></p>
+                    <div className="link_and_button_line">
+                        <p><a href="/overview" className="return_home_link"><CaretLeft />Terug naar de overzichtspagina</a></p>
+                        <button onClick={deletePost} className="delete_button"><X />Verwijder post</button>
+                    </div>
                 </article>
+            </div>}
+            {deleteMessage && <div className="inner-container">
+                <h2 className="message">De post is succesvol verwijderd.</h2>
             </div>}
         </div>
     )
